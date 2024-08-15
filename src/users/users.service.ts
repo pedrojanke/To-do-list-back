c02e375828c3e438aa136ef7c2cb4f6913a187d2
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-08-15 20:16:03 -0300

    realizo o crud da tabela users

---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const user = this.userRepository.create({
+            ...createUserDto,
+            created_at: new Date(),
+            updated_at: new Date(),
+        });
+        return this.userRepository.save(user);
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    async findOne(id: number): Promise<User> {
+        return this.userRepository.findOneBy({ id });
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+        await this.userRepository.update(id, updateUserDto);
+        return this.userRepository.findOneBy({ id });
+    }
+}
